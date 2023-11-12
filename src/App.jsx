432b6672f8c20d668e4d33cd229b065e850ded36
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-11-12 11:55:03 +0100

    :tada: Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  return(
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>    
+  )
+}
+
+export default App
