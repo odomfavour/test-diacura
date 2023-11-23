289133d1f92a4342aaf7a5815b2e720b90c91338
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-11-23 16:28:26 +0100

    :construction: ft: Completed Reset Password Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return(
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
     </Routes>    
   )
 }
