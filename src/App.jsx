8f3f4af125f5a6fdf32888f9636ceeac80d47409
e4ma-okafor <ifeomasandra13@gmail.com>
2023-11-23 03:40:25 +0100

    created get-started page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import GetStarted from "./pages/GetStarted";
 
 const App = () => {
   return(
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />
+      <Route path="/get-started" element={<GetStarted />} />
     </Routes>    
   )
 }
