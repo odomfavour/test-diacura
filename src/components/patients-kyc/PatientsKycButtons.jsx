0e957f90ad72982f274f9aaf043fdf398393364d
Austinet <austinetsplendour@gmail.com>
2023-11-23 17:35:40 +0100

    fx: changed button width for small screens

--- src/components/patients-kyc/PatientsKycButtons.jsx
+++ src/components/patients-kyc/PatientsKycButtons.jsx
@@ -7,14 +7,14 @@ const PatientsKycButtons = ({ previous }) => {
       <Link to={`/${previous}`}>
         <button
           type="button"
-          className="w-[10rem] md:w-[17.0625rem] h-[3rem] md:h-[3.5rem] rounded-[0.25rem] border border-primary-color-light-blue-300 text-primary-color-light-blue-300 font-bold text-[1.25rem] hover:text-white hover:bg-primary-color-light-blue-300 transition-all duration-300 ease-in-out"
+          className="w-[8rem] md:w-[17.0625rem] h-[3rem] md:h-[3.5rem] rounded-[0.25rem] border border-primary-color-light-blue-300 text-primary-color-light-blue-300 font-bold text-[1.25rem] hover:text-white hover:bg-primary-color-light-blue-300 transition-all duration-300 ease-in-out"
         >
           Back
         </button>
       </Link>
       <button
         type="submit"
-        className="w-[10rem] md:w-[17.0625rem] h-[3rem] md:h-[3.5rem] rounded-[0.25rem] border border-primary-color-light-blue-300 text-white font-bold text-[1.25rem] bg-primary-color-light-blue-300 hover:text-primary-color-light-blue-300 hover:bg-transparent"
+        className="w-[8rem] md:w-[17.0625rem] h-[3rem] md:h-[3.5rem] rounded-[0.25rem] border border-primary-color-light-blue-300 text-white font-bold text-[1.25rem] bg-primary-color-light-blue-300 hover:text-primary-color-light-blue-300 hover:bg-transparent"
       >
         Next
       </button>
