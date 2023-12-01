a4d0dd95b6e5f7a02910c34781985f302dc18c4e
Austinet <austinetsplendour@gmail.com>
2023-12-01 19:59:50 +0100

    Revert "ft: add chate pages"
    This reverts commit dde48d817eccfdc60f8ececeb18d55cadb8f81a3.

--- src/components/dashboards/patients/ChatTab.jsx
+++ src/components/dashboards/patients/ChatTab.jsx
@@ -1,23 +1,9 @@
+
+
 const ChatTab = () => {
   return (
-    <section>
-      <div className="p-[0.75rem]">
-        {/* Chat tabs */}
-        <ul className="px-[1rem] flex items-center gap-[1.84rem]">
-          <li>
-            <button type="button" className={`text-[#0D67A0] text-[0.875rem] font-bold leading-normal`}>All</button>
-          </li>
-          <li>
-            <button type="button" className={`text-[#262626] text-[0.875rem] font-semibold leading-normal`}>Read</button>
-          </li>
-          <li>
-            <button type="button" className={`text-[#262626] text-[0.875rem] font-semibold leading-normal`}>Unread</button>
-          </li>
-        </ul>
-      </div>
-      <div></div>
-    </section>
-  );
-};
+    <div>Chat</div>
+  )
+}
 
-export default ChatTab;
+export default ChatTab
