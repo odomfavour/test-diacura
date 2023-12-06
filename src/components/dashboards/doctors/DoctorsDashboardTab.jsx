ad5e34797880f6a077b662e276d47c49d8c47f6a
fiiwebarry <fiiwebarry@yahoo.com>
2023-12-06 18:30:08 +0100

    update

--- src/components/dashboards/doctors/DoctorsDashboardTab.jsx
+++ src/components/dashboards/doctors/DoctorsDashboardTab.jsx
@@ -47,41 +47,54 @@ const DoctorsDashboardTab = () => {
 
           </div>
 
-          <div className="shadow-lg lg:w-[961px] md:w-[361px] lg:h-[287px] md:h-[180px] bg-[#FFFF] rounded-xl lg:mt-5">
-            <div className="flex">
-              <div className="flex">
+          <div className="shadow-lg lg:w-[961px] md:w-[361px] lg:h-[287px] md:h-[180px] bg-[#FFFF] rounded-xl p-5 lg:mt-5">
+            <div className="flex justify-between">
+              <div className="flex gap-2">
                 <div>
                   Upcoming  appointments
                 </div>
-                <div>10</div>
+                <div className="w-[14px] h-[16px] bg-[#CFE5F2] rounded ">10</div>
               </div>
               <div className="flex">
+                <p>See More</p>
 
               </div>
 
             </div>
-            <div className="flex">
-              <div className="flex">
-                <div>
-                  <img className="" src={doc} alt="doc" />
+            <div className="flex gap-[43px]">
+              <div >
+                <div className="flex gap-[30px]">
+                  <div>
+                    <img className="" src={doc} alt="doc" />
+                  </div>
+                  <div>
+                    <p className="font-open-sans lg:text-[16px] font-bold mt-2">Mrs. Promise Okohlame</p>
+                    <p className="font-open-sans lg:text-[12px] font-semibold  mt-2">Suffering from diabetic Type-A</p>
+                    <p className="font-open-sans lg:text-[10px] font-semibold  mt-2">Age: 45 years  | Sex: Female  </p>
+                  </div>
+
                 </div>
-                <div>
-                  <p className="font-open-sans lg:text-[16px] font-bold mt-2">Mrs. Promise Okohlame</p>
-                  <p className="font-open-sans lg:text-[12px] font-semibold  mt-2">Suffering from diabetic Type-A</p>
-                  <p className="font-open-sans lg:text-[10px] font-semibold  mt-2">Age: 45 years  | Sex: Female  </p>
+                <div className=" flex gap-5 mt-5">
+                  <button className="font-open-sans font-bold text-[14px] w-[211px] h-[40px] text-[#EB3434] rounded bg-[#FFF5F5]">Cancel appointment</button>
+                  <button className="font-open-sans font-bold text-[14px]  w-[211px] h-[40px] text-[#FFFFFF] rounded bg-[#107BC0]">Reschedule appointment</button>
                 </div>
-                <div></div>
               </div>
-              <div className="flex">
-                <div>
-                  <img className="" src={doc} alt="doc" />
+              <div >
+                <div className="flex gap-[34px]">
+                  <div>
+                    <img className="" src={doc} alt="doc" />
+                  </div>
+                  <div>
+                    <p className="font-open-sans lg:text-[16px] font-bold mt-2">Mrs. Promise Okohlame</p>
+                    <p className="font-open-sans lg:text-[12px] font-semibold  mt-2">Suffering from diabetic Type-A</p>
+                    <p className="font-open-sans lg:text-[10px] font-semibold  mt-2">Age: 45 years  | Sex: Female  </p>
+                  </div>
+
                 </div>
-                <div>
-                  <p className="font-open-sans lg:text-[16px] font-bold mt-2">Mrs. Promise Okohlame</p>
-                  <p className="font-open-sans lg:text-[12px] font-semibold  mt-2">Suffering from diabetic Type-A</p>
-                  <p className="font-open-sans lg:text-[10px] font-semibold  mt-2">Age: 45 years  | Sex: Female  </p>
+                <div className="mt-5 flex gap-3">
+                  <button className="font-open-sans font-bold text-[14px] rounded w-[211px] h-[40px] text-[#EB3434] bg-[#FFF5F5]">Cancel appointment</button>
+                  <button className="font-open-sans font-bold text-[14px]  rounded  w-[211px] text-[#FFFFFF] h-[40px] bg-[#107BC0]">Reschedule appointment</button>
                 </div>
-                <div></div>
               </div>
             </div>
 
